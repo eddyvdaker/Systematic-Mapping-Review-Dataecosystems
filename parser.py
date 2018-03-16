597bd042551197f636ec5f3d292a0d8766680f18
Eddy van den Aker <eddy.v.d.aker@gmail.com>
2018-03-16 14:51:50 +0100

    finished xml parser and writing to json

--- parser.py
+++ parser.py
@@ -4,6 +4,7 @@ the results to json and csv (for easy import into spreadsheets) files.
 """
 
 import bs4
+import json
 
 
 def read_files(files):
@@ -28,25 +29,27 @@ def get_publish_date(parser):
     return f'{publish_date["year"]}-{publish_date["month"]}-{publish_date["day"]}'
 
 
-heading = ['id', 'query', 'in_query_id', 'title', 'author', 'publish_date', 'journal', 'publisher',
-           'article_or_conference-proceding', 'database', 'url', 'language',
-           'abstract']
 test_files = ['./results/single-result.xml', './results/single-result.xml']
 result_files = ['./results/q1-results.xml', './results/q2-results.xml', './results/q3-results.xml']
-files = test_files
+files = result_files
 
-
+print('Reading original files...')
 xml = read_files(files)
-parser = bs4.BeautifulSoup(xml, 'lxml')
-id = 0
-json_results = {}
 
+print('Setting up parser...')
+parser = bs4.BeautifulSoup(xml, 'lxml')
+entries = []
+rec_nr = 0
+
+print('Start parsing entries...')
 for i in range(len(files)):
     query = str(parser.find(f'q{i}'))
     query_parser = bs4.BeautifulSoup(query, 'lxml')
 
     records = [str(x) for x in query_parser.findAll('rec')]
     for record in records:
+        rec_nr += 1
+        print(f'currently at record {rec_nr}')
         rec_parser = bs4.BeautifulSoup(record, 'lxml')
 
         title = str(rec_parser.find('atl'))[5:-6]
@@ -56,8 +59,21 @@ for i in range(len(files)):
         database = str(rec_parser.find('header')['longdbname'])
         url = str(rec_parser.find('url'))[5:-6]
         language = str(rec_parser.find('language'))[21:-11]
-        abstract = str(rec_parser.find('ab'))[4:-5]
         journal = str(rec_parser.find('jtl'))[5:-6]
-        
+        abstract = str(rec_parser.find('ab'))[4:-5]
 
+        entries.append({
+            'title': title,
+            'authors': authors,
+            'publish_date': publish_date,
+            'publication_type': publication_type,
+            'database': database,
+            'url': url,
+            'language': language,
+            'journal': journal,
+            'abstract': abstract
+        })
 
+print('writing json results...')
+with open('./results/results.json', 'w') as output:
+    json.dump({'results': entries}, output, sort_keys=True, indent=4)
